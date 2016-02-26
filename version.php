fa7a6aa5c8c3359c2566325bc3be0d36b65485c9
Eric Bjella <eric.bjella@remote-learner.net>
2016-02-25 17:05:59 -0700

    HOSSUP-6775 - Initial build of Altitude theme.

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    theme_altitude
+ * @copyright  2016 Moodle, moodle.org
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2014111000;
+$plugin->requires  = 2014110400;
+$plugin->release   = '2.8.1';
+$plugin->component = 'theme_altitude';
+$plugin->dependencies = array(
+    'theme_bootstrapbase'  => 2014111000,
+    'theme_clean'  => 2014111000
+);
