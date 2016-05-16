62bf2bc3ed17f1bdac30ab922fbd468c18397872
Derek Henderson <derek.henderson@remote-learner.net>
2016-05-16 11:03:22 -0400

    AMT-13 Add my courses menu option to the theme. Updated language file,        settings, and layouts file.

--- layout/embedded.php
+++ layout/embedded.php
@@ -33,6 +33,7 @@ echo $OUTPUT->doctype() ?>
 
 <body <?php echo $OUTPUT->body_attributes(); ?>>
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
+
 <div id="page">
     <div id="page-content" class="clearfix">
         <?php echo $OUTPUT->main_content(); ?>
