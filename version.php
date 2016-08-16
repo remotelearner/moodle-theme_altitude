7cfd4f7215011991a779be396d9fd5cedda683aa
Eric Bjella <eric.bjella@remote-learner.net>
2016-08-15 21:35:37 -0600

    Updated release to 3.1.0.1

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016052300;
+$plugin->version   = 2016052301;
 $plugin->requires  = 2016052300;
-$plugin->release   = '3.1.0.0';
+$plugin->release   = '3.1.0.1';
 $plugin->component = 'theme_altitude';
 $plugin->dependencies = array(
     'theme_bootstrapbase' => 2016052300,
